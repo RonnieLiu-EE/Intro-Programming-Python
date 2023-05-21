e16228a8e9ef51ca278df589d622c9dcbb57d781
Ronnie Liu <ronnieliou1989@gmail.com>
2023-05-21 13:10:14 -0500

    Added item.

---
+++ Section2/how_old_am_i.py
@@ -0,0 +1,19 @@
+# Ask user to provide the current year,
+# then store it in variable current_year.
+current_year = # REPLACE COMMENT WITH YOUR CODE HERE
+
+# Ask user to provide their birth year,
+# then store it in variable birth_year.
+birth_year = # REPLACE COMMENT WITH YOUR CODE HERE
+
+# Convert string type to int type to allow mathematic calculation.
+# Don't worry if you don't understand this part, as we haven't 
+# covered data type and conversion yet. We'll revisit in future.
+current_year = int(current_year)
+birth_year = int(birth_year)
+
+# Calculate age = current_year - birth_year
+age = # REPLACE COMMENT WITH YOUR CODE HERE
+
+# Output result
+print(f"Based on the info you provided, you're {age} years old!")
---
+++ Section2/user_input.py
@@ -0,0 +1,7 @@
+# Ask user to provide input,
+# then store it in variable user_input.
+user_input = input("Type anything, then hit ENTER: ")
+
+# Outputs user inpurt on the screen.
+print("User input: " + user_input)
+
