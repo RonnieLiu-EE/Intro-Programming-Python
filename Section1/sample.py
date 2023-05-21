97348ffb96c918ada2d4ac8042202311cb3bba43
Ronnie Liu <ronnieliou1989@gmail.com>
2023-05-21 09:23:49 -0500

    Added item.

---
+++ Section1/sample.py
@@ -0,0 +1,10 @@
+# Assign a numeric value
+passing_score = 70
+student_scores = [79, 68, 90, 55, 70]
+
+# Check the is more than 70 or not
+for score in student_scores:
+    if (score >= 70):
+        print("You have passed")
+    else:
+        print("You have not passed")
